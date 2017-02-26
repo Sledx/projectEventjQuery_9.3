a25695abde91174f38fbb66287bcfa3c63adf7d7
Figlarz <Figlarz@Figlarzs-MacBook-Pro.local>
2017-02-26 11:33:20 +0100

    fixed

--- scripts.js
+++ scripts.js
@@ -1,18 +1,13 @@
 $(function(){
-    
-var carouselList = $("#carousel ul");
-
-function moveFirstSlide() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
-    lastItem.after(firstItem);
-    carouselList.css({marginLeft:0});
-};
-    
-function changeSlides() {
-    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-};
-
-setInterval(changeSlides, 3000);
-        
+    var carouselList = $("#carousel ul");
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+    }
+    function changeSlides() {
+        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+    }
+    setInterval(changeSlides, 3000);    
 });
